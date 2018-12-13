4e5dc05ef691c81ab13f4794bab7b7939add0ff1
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2018-12-13 15:29:38 -0800

    Completed main exercise

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const APIError = require('../errors');
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
@@ -8,29 +9,35 @@ router.use(express.urlencoded({ extended: true }));
 router.get('/', async (request, response, next) => {
   try {
     var results = await db.query('SELECT code, name FROM companies');
+    return response.json({ companies: results.rows });
   } catch (error) {
     return next(error);
   }
-
-  return response.json({ companies: results.rows });
 });
 
 router.use('/:code', async function checkIfCodeExists(request, response, next) {
   try {
     var results = await db.query(
-      'SELECT code, name, description FROM companies WHERE code=$1',
+      'SELECT * FROM companies c JOIN invoices i ON i.comp_code = c.code WHERE code=$1',
       [request.params.code]
     );
+    if (results.rows.length === 0) {
+      throw new APIError(`Company not found for ${request.params.code}`, 404);
+    }
+    let { name, description, code } = results.rows[0];
+    request.company = {
+      name,
+      description,
+      code
+    };
+    request.company.invoices = results.rows.map(item => {
+      let { name, description, code, comp_code, id, ...invoice } = item;
+      return invoice;
+    });
+    return next();
   } catch (error) {
     return next(error);
   }
-  if (results.rows.length === 0) {
-    let error = new Error(`Company not found for ${request.params.code}`);
-    error.status = 404;
-    return next(error);
-  }
-  request.company = results.rows[0];
-  return next();
 });
 
 router.get('/:code', (request, response, next) => {
@@ -45,10 +52,11 @@ router.put('/:code', async (request, response, next) => {
       `UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING *`,
       [name, description, request.company.code]
     );
+    result.rows[0].invoices = request.company.invoices;
+    return response.json({ company: result.rows[0] });
   } catch (error) {
     return next(error);
   }
-  return response.json(result.rows[0]);
 });
 
 router.delete('/:code', async (request, response, next) => {
@@ -57,25 +65,23 @@ router.delete('/:code', async (request, response, next) => {
       'DELETE FROM companies WHERE code=$1 RETURNING *',
       [request.params.code]
     );
+    return response.json({ status: 'deleted', company: request.company });
   } catch (error) {
     next(error);
   }
-  return response.json({ status: 'deleted', company: result.rows[0] });
 });
 
 router.post('/', async (request, response, next) => {
-  let { code, name, description } = request.body;
-
   try {
+    let { code, name, description } = request.body;
     var results = await db.query(
       'INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING *',
       [code, name, description]
     );
+    return response.json({ company: results.rows[0] });
   } catch (error) {
     return next(error);
   }
-
-  return response.json({ company: results.rows[0] });
 });
 
 module.exports = router;
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const APIError = require('../errors');
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
@@ -8,10 +9,10 @@ router.use(express.urlencoded({ extended: true }));
 router.get('/', async (request, response, next) => {
   try {
     var results = await db.query('SELECT id, comp_code FROM invoices');
+    return response.json({ invoices: results.rows });
   } catch (error) {
     return next(error);
   }
-  return response.json({ invoices: results.rows });
 });
 
 router.use('/:id', async function checkIfidExists(request, response, next) {
@@ -20,23 +21,21 @@ router.use('/:id', async function checkIfidExists(request, response, next) {
       'SELECT i.id, i.amt, i.paid, i.add_date, i.paid_date, c.code, c.name, c.description FROM invoices i JOIN companies c ON i.comp_code = c.code WHERE i.id=$1',
       [request.params.id]
     );
+    if (results.rows.length === 0) {
+      throw new APIError(`Invoice not found for ${request.params.id}`, 404);
+    }
+    let { code, name, description, ...invoice } = results.rows[0];
+    request.company = {
+      code,
+      name,
+      description
+    };
+    invoice.company = request.company;
+    request.invoice = invoice;
+    return next();
   } catch (error) {
     return next(error);
   }
-  if (results.rows.length === 0) {
-    let error = new Error(`Invoice not found for ${request.params.id}`);
-    error.status = 404;
-    return next(error);
-  }
-  let { code, name, description, ...invoice } = results.rows[0];
-  request.company = {
-    code,
-    name,
-    description
-  };
-  invoice.company = request.company;
-  request.invoice = invoice;
-  return next();
 });
 
 router.get('/:id', (request, response, next) => {
@@ -45,16 +44,15 @@ router.get('/:id', (request, response, next) => {
 
 router.put('/:id', async (request, response, next) => {
   try {
-    let name = request.body.name || request.company.name;
-    let description = request.body.description || request.company.description;
+    let amt = request.body.amt || request.invoice.amt;
     var result = await db.query(
-      `UPDATE companies SET name=$1, description=$2 WHERE id=$3 RETURNING *`,
-      [name, description, request.company.id]
+      `UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING *`,
+      [amt, request.invoice.id]
     );
+    return response.json({ invoice: result.rows[0] });
   } catch (error) {
     return next(error);
   }
-  return response.json(result.rows[0]);
 });
 
 router.delete('/:id', async (request, response, next) => {
@@ -63,25 +61,22 @@ router.delete('/:id', async (request, response, next) => {
       'DELETE FROM invoices WHERE id=$1 RETURNING *',
       [request.params.id]
     );
+    return response.json({ status: 'deleted', invoice: result.rows[0] });
   } catch (error) {
     next(error);
   }
-  return response.json({ status: 'deleted', company: result.rows[0] });
 });
 
 router.post('/', async (request, response, next) => {
-  let { id, name, description } = request.body;
-
   try {
     var results = await db.query(
-      'INSERT INTO invoices (id, name, description) VALUES ($1, $2, $3) RETURNING *',
-      [id, name, description]
+      'INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING *',
+      [request.body.comp_code, request.body.amt]
     );
+    return response.json({ invoice: results.rows[0] });
   } catch (error) {
     return next(error);
   }
-
-  return response.json({ company: results.rows[0] });
 });
 
 module.exports = router;
